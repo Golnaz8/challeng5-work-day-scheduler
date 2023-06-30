2176c7a28d73fac12319246fd4b5c70ce5ed1937
Golnaz8 <golnaz.brj@gmail.com>
2023-06-30 17:02:37 -0400

    added comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,17 @@ var today = dayjs();
 
 var hours = ['9', '10', '11', '12', '13', '14', '15', '16', '17']; 
 
+//display the time in the format that it asked.
 var date = today.format('dddd, MMMM-D, YYYY');
 currentDayEl.text(date);
+
+//keep current time 's hour
 var thisHour = today.format('H');
 var currentHourLastTime = thisHour;
 var thisHourNum = parseInt(thisHour);
 
+//if current time is in the work shift hour then just this hour should be red as present time
+//all hours before that should be gray as past time and all hours after that should be green as future time 
 if (thisHourNum>8 && thisHourNum<18) {
     console.log(`true`);
     var index = hours.indexOf(thisHour);
@@ -36,7 +41,8 @@ if (thisHourNum>8 && thisHourNum<18) {
         beforePresent(i);
     }
 }
-    
+
+//function to make the schedule in particular range gray as past time
 function beforePresent(num) {
     var choose = containerLgEl.children().eq(num);
     console.log(choose);
@@ -45,6 +51,7 @@ function beforePresent(num) {
     choose.removeClass('present');
 }
 
+//function to make the schedule in particular range green as future time
 function afterPresent(num) {
     var choose = containerLgEl.children().eq(num);
     console.log(choose);
@@ -53,6 +60,7 @@ function afterPresent(num) {
     choose.removeClass('present');
 }
 
+//function to check if the hour changed, reload the page to change the time block boxes 
 function refreshPageWhenHourChanged(){
     var currentDay = dayjs();
     var currentHour = currentDay.format('H');
@@ -63,8 +71,10 @@ function refreshPageWhenHourChanged(){
     currentHourLastTime = currentHour;
 }
  
+//each second calls this function
 setInterval(refreshPageWhenHourChanged,1000);
 
+//function to retrieve userText from local storage 
 function retrieveFromLocalStorage() {
     var savedUserText = JSON.parse(localStorage.getItem('userText'));
 
@@ -74,10 +84,12 @@ function retrieveFromLocalStorage() {
     return savedUserText;
 }
 
+//function to save to local storage
 function saveToLocalStorage(userText) {
     localStorage.setItem('userText', JSON.stringify(userText));
 }
 
+//function to fill description with saved user input
 function renderAfterLoad() {
     var savedUserTexts = retrieveFromLocalStorage();
   
@@ -85,31 +97,39 @@ function renderAfterLoad() {
       var timeIdEl = document.getElementById(`hour-${i}`);
       var textInput = timeIdEl.querySelector('.description');
       
+      //Find the saved userText for the current time id(like hour-12)
       var savedText = savedUserTexts.find(function (event) {
         return event.time === `hour-${i}`;
       });
+
+      // If a saved userText is found, set the input value to the saved userText text
       if (savedText) {
         textInput.value = savedText.text;
       }
     }
 }
 
+//event listener function when save button clicked
 saveBtnEl.on('click',function() {
     var savedUserTexts = retrieveFromLocalStorage();
     console.log(savedUserTexts);
     var time = $(this).parent().attr("id");
     var text = $(this).siblings(".description").val().trim();
     
+    //find the index of saved userText for the current time id
     var textIndex = savedUserTexts.findIndex(function (event) {
         return event.time === time;
     });
-  
+    
+    //if there is a saved text, update the text
     if (textIndex !== -1) {
         savedUserTexts[textIndex].text = text;
+      //if not, add a new to the array  
     } else {
         savedUserTexts.push({ time: time, text: text });
     }
-  
+    
+    //update the local storage
     saveToLocalStorage(savedUserTexts);
 });
 
@@ -117,6 +137,7 @@ function init() {
     renderAfterLoad();
 }
 
+//call init function when the DOM is ready
 $(document).ready(init);
 
 
